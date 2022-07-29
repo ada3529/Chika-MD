9b44328745530dfa9d06d7aa30a59375742cfaf2
DUCKTys <110259080+DUCKTys@users.noreply.github.com>
2022-07-30 02:37:10 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,9 +27,9 @@ global.premium = ['6281575886399','6281359053674'] //ubah jadi nomor mu, note ta
 global.ownernomer = '6281359053674' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'T0MTys' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'MD Projects' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'T0MTys' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Tanaka' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Chika-MD' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'DuckTys' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
@@ -41,8 +41,8 @@ global.email = 'projectthenext1532@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© T0MTys' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'T0MTys' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© DuckTys' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'Tanaka' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
